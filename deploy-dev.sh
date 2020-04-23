3529ac100741d59aced33f3dee247caf68c5a273
Duy <qduy88@gmail.com>
2020-04-23 10:50:24 +0700

    Update CI

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -20,19 +20,19 @@ chmod 600 key.pem
 ##Set up 3 container
 
 export APP_CMD="docker login $REGISTRY_URL -u $REGISTRY_USER -p $REGISTRY_PASS;
-        docker volume rm virustracker;
-        docker stop ${IMAGE_NAME}-api-${IMAGE_TAG};
-        docker stop ${IMAGE_NAME}-worker-${IMAGE_TAG};
+        docker volume rm virustracker-crawler;
+        docker stop virustracker-crawler-api-${IMAGE_TAG};
+        docker stop virustracker-crawler-worker-${IMAGE_TAG};
         docker pull ${REGISTRY_URL}/${IMAGE_NAME}:${IMAGE_TAG}; 
         docker pull ${REGISTRY_URL}/${IMAGE_NAME}-api:${IMAGE_TAG}; 
         docker pull ${REGISTRY_URL}/${IMAGE_NAME}-worker:${IMAGE_TAG}; 
-        docker run --name virustracker-${IMAGE_TAG} --mount source=virustracker,target=/opt/cs50vn/virustracker --rm --network host -d ${REGISTRY_URL}/${IMAGE_NAME}:${IMAGE_TAG};
-        docker run --name virustracker-api-${IMAGE_TAG} --mount source=virustracker,target=/opt/cs50vn/virustracker --rm --network host -d ${REGISTRY_URL}/${IMAGE_NAME}-api:${IMAGE_TAG};
-        docker run --name virustracker-worker-${IMAGE_TAG} --mount source=virustracker,target=/opt/cs50vn/virustracker --rm --network host -d ${REGISTRY_URL}/${IMAGE_NAME}-worker:${IMAGE_TAG};
+        docker run --name virustracker-crawler-${IMAGE_TAG} --mount source=virustracker,target=/opt/cs50vn/virustracker --rm --network host -d ${REGISTRY_URL}/${IMAGE_NAME}:${IMAGE_TAG};
+        docker run --name virustracker-crawler-api-${IMAGE_TAG} --mount source=virustracker,target=/opt/cs50vn/virustracker --rm --network host -d ${REGISTRY_URL}/${IMAGE_NAME}-api:${IMAGE_TAG};
+        docker run --name virustracker-crawler-worker-${IMAGE_TAG} --mount source=virustracker,target=/opt/cs50vn/virustracker --rm --network host -d ${REGISTRY_URL}/${IMAGE_NAME}-worker:${IMAGE_TAG};
 
-        docker stop virustracker-${IMAGE_TAG};
+        docker stop virustracker-crawler-${IMAGE_TAG};
         docker images;
-        docker inspect virustracker;
-        
+        docker inspect virustracker-crawler;
+
         docker ps -a" 
 ssh -i key.pem -o StrictHostKeyChecking=no $DEPLOY_SERVER_USER@$DEPLOY_SERVER_URL $APP_CMD
