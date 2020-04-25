a17998c88260c6793574a52259457b476c81ad61
Duy <qduy88@gmail.com>
2020-04-25 08:51:25 +0700

    Update

--- go/src/worker/main.go
+++ go/src/worker/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-    "crypto/tls"
     "cs50vn/virustracker/crawler/worker/apprepository"
     "cs50vn/virustracker/crawler/worker/apprepository/model"
     "cs50vn/virustracker/crawler/worker/utils"
     "database/sql"
     "encoding/json"
     "fmt"
+    "github.com/PuerkitoBio/goquery"
     "github.com/jasonlvhit/gocron"
     _ "github.com/mattn/go-sqlite3"
     "io/ioutil"
     "net/http"
+    "strconv"
     "strings"
+    "time"
 )
 
 func LoadConfig() {
@@ -75,99 +77,95 @@ func ProcessJob() {
     apprepository.CountryList = make([]*model.Item, 0)
     //
     //Call url to process
-    tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-    client := &http.Client{Transport: tr}
-    res, err := client.Get("https://worldometers.info/coronavirus")
-    //res, err := http.Get("https://" + apprepository.Config.CrawUrl)
+    //res, err := http.Get("https://worldometers.info/coronavirus")
+    res, err := http.Get("https://" + apprepository.Config.CrawUrl)
     if err != nil {
       fmt.Println(err.Error())
     }
     defer res.Body.Close()
 
     //Parse data
-    //doc, err := goquery.NewDocumentFromReader(res.Body)
-    //if err != nil {
-    //  fmt.Println(err.Error())
-    //}
-    //now := time.Now().Unix()
-    //
-    //count := 0
-    //doc.Find("table#main_table_countries_today tbody tr").Each(func(i int, s *goquery.Selection) {
-    //  nodes := s.Find("td")
-    //  firstNode := nodes.Eq(0)
-    //
-    //  //fmt.Println(node.Text())
-    //  if value, ok := apprepository.MappingCountryList[strings.TrimSpace(firstNode.Text())]; ok {
-    //      count++
-    //      leftName := nodes.Eq(0).Text()
-    //      rightName := value
-    //      totalCases, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(1).Text(), ",", "")), 10, 64)
-    //      if err != nil {
-    //          totalCases = 0
-    //      }
-    //      totalDeaths, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(3).Text(), ",", "")), 10, 64)
-    //      if err != nil {
-    //          totalDeaths = 0
-    //      }
-    //      totalRecovered, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(5).Text(), ",", "")), 10, 64)
-    //      if err != nil {
-    //          totalRecovered = 0
-    //      }
-    //      seriousCases, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(7).Text(), ",", "")), 10, 64)
-    //      if err != nil {
-    //          seriousCases = 0
-    //      }
-    //      totalCasesPer1Pop, err := strconv.ParseFloat(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(8).Text(), ",", "")), 32)
-    //      if err != nil {
-    //          totalCasesPer1Pop = 0.0
-    //      }
-    //      totalDeathsPer1Pop, err := strconv.ParseFloat(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(9).Text(), ",", "")), 32)
-    //      if err != nil {
-    //          totalDeathsPer1Pop = 0.0
-    //      }
-    //      totalTests, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(10).Text(), ",", "")), 10, 64)
-    //      if err != nil {
-    //          totalTests = 0
-    //      }
-    //      testsPer1Pop, err := strconv.ParseFloat(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(11).Text(), ",", "")), 32)
-    //      if err != nil {
-    //          testsPer1Pop = 0.0
-    //      }
-    //      timestamp := now
-    //
-    //      apprepository.CountryList = append(apprepository.CountryList, model.MakeItem(leftName, rightName, totalCases, totalDeaths, totalRecovered, seriousCases, totalCasesPer1Pop, totalDeathsPer1Pop, totalTests, testsPer1Pop, timestamp))
-    //
-    //  }
-    //})
+    doc, err := goquery.NewDocumentFromReader(res.Body)
+    if err != nil {
+     fmt.Println(err.Error())
+    }
+    now := time.Now().Unix()
 
-    //fmt.Println("Total nodes: ", count)
-    //
-    //for _, item := range apprepository.CountryList {
-    //   fmt.Println(item)
-    //}
+    count := 0
+    doc.Find("table#main_table_countries_today tbody tr").Each(func(i int, s *goquery.Selection) {
+     nodes := s.Find("td")
+     firstNode := nodes.Eq(0)
 
-    ////Write data to db
-    //tx, err := apprepository.DbConnection.Begin()
-    //if err != nil {
-    //    fmt.Println(err.Error())
-    //}
-    //preparedInsert := utils.SQL_INSERT_TRACKING_ITEM
-    //
-    //preparedSt, err := apprepository.DbConnection.Prepare(preparedInsert)
-    //if err != nil {
-    //    fmt.Println(err.Error())
-    //}
-    //
-    //for _, item := range apprepository.CountryList {
-    //    _, err := preparedSt.Exec(item.LeftName, item.RightName, item.TotalCases, item.TotalDeaths, item.TotalRecovered, item.SeriousCases, item.TotalCasesPer1Pop, item.TotalDeathsPer1Pop, item.TotalTests, item.TestsPer1Pop, item.Timestamp)
-    //    if err != nil {
-    //        fmt.Println(err.Error())
-    //    }
-    //}
-    //
-    //tx.Commit()
+     //fmt.Println(node.Text())
+     if value, ok := apprepository.MappingCountryList[strings.TrimSpace(firstNode.Text())]; ok {
+         count++
+         leftName := nodes.Eq(0).Text()
+         rightName := value
+         totalCases, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(1).Text(), ",", "")), 10, 64)
+         if err != nil {
+             totalCases = 0
+         }
+         totalDeaths, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(3).Text(), ",", "")), 10, 64)
+         if err != nil {
+             totalDeaths = 0
+         }
+         totalRecovered, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(5).Text(), ",", "")), 10, 64)
+         if err != nil {
+             totalRecovered = 0
+         }
+         seriousCases, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(7).Text(), ",", "")), 10, 64)
+         if err != nil {
+             seriousCases = 0
+         }
+         totalCasesPer1Pop, err := strconv.ParseFloat(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(8).Text(), ",", "")), 32)
+         if err != nil {
+             totalCasesPer1Pop = 0.0
+         }
+         totalDeathsPer1Pop, err := strconv.ParseFloat(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(9).Text(), ",", "")), 32)
+         if err != nil {
+             totalDeathsPer1Pop = 0.0
+         }
+         totalTests, err := strconv.ParseInt(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(10).Text(), ",", "")), 10, 64)
+         if err != nil {
+             totalTests = 0
+         }
+         testsPer1Pop, err := strconv.ParseFloat(strings.TrimSpace(strings.ReplaceAll(nodes.Eq(11).Text(), ",", "")), 32)
+         if err != nil {
+             testsPer1Pop = 0.0
+         }
+         timestamp := now
+
+         apprepository.CountryList = append(apprepository.CountryList, model.MakeItem(leftName, rightName, totalCases, totalDeaths, totalRecovered, seriousCases, totalCasesPer1Pop, totalDeathsPer1Pop, totalTests, testsPer1Pop, timestamp))
+
+     }
+    })
+
+    fmt.Println("Total nodes: ", count)
+
+    for _, item := range apprepository.CountryList {
+      fmt.Println(item)
+    }
+
+    //Write data to db
+    tx, err := apprepository.DbConnection.Begin()
+    if err != nil {
+       fmt.Println(err.Error())
+    }
+    preparedInsert := utils.SQL_INSERT_TRACKING_ITEM
+
+    preparedSt, err := apprepository.DbConnection.Prepare(preparedInsert)
+    if err != nil {
+       fmt.Println(err.Error())
+    }
+
+    for _, item := range apprepository.CountryList {
+       _, err := preparedSt.Exec(item.LeftName, item.RightName, item.TotalCases, item.TotalDeaths, item.TotalRecovered, item.SeriousCases, item.TotalCasesPer1Pop, item.TotalDeathsPer1Pop, item.TotalTests, item.TestsPer1Pop, item.Timestamp)
+       if err != nil {
+           fmt.Println(err.Error())
+       }
+    }
+
+    tx.Commit()
 
 }
 
@@ -185,7 +183,7 @@ func InitApp() {
     LoadConfig()
     InitDb()
 
-    gocron.Every(10).Seconds().Do(HandleJob)
+    gocron.Every(apprepository.Config.Times).Seconds().Do(HandleJob)
 }
 
 func main() {
